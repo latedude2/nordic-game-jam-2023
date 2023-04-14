69d76c3aea7a5075a939a598d7393ea798c0b333
latedude2 <simasc10@gmail.com>
2023-04-14 22:53:39 +0200

    WIP move camera when rotating

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera camera;
+    private Vector3 cameraPosition;
+    [SerializeField] private float cameraSpeed = 1000f;
     void Start()
     {
+        camera = GetComponent<Camera>();
+        cameraPosition = camera.transform.parent.localPosition;
+        //lock mouse
+        Cursor.lockState = CursorLockMode.Locked;
         
     }
 
-    // Update is called once per frame
+    // Rotate camera using mouse 
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.RotateAround(transform.position, Vector3.up, mouseX * cameraSpeed * Time.deltaTime);
+        transform.RotateAround(transform.position, transform.right, -mouseY * cameraSpeed * Time.deltaTime);
+
+        //stop rotation on the z axis
+        Vector3 euler = transform.eulerAngles;
+        euler.z = 0;
+        transform.eulerAngles = euler;
+
+        //move camera
+        float moveX = transform.localRotation.eulerAngles.y;
+        //
+  
+        transform.parent.localPosition = cameraPosition + new Vector3(1,0,0) * moveX * 0.01f;
+
+
+
         
     }
+
+    
 }
