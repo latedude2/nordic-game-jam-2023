9abb12d1aa362fc043e0e774b154e74c0cd0c173
latedude2 <simasc10@gmail.com>
2023-04-16 12:28:35 +0200

    Unlock mouse in end screen

---
+++ Assets/Scripts/UI/UnlockMouse.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //unlock mouse
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
