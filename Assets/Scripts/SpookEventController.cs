f0fb8994a566be90ca146b3c11f6155e3de89221
Mikkel Munk <cloaked@live.dk>
2023-04-16 13:06:10 +0200

    wow decals

---
+++ Assets/Scripts/SpookEventController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpookEventController : MonoBehaviour
+{
+    public int spookOmeter = 0;
+    float timePassed = 0f;
+
+    [SerializeField] GameObject[] decals;
+
+    void Start()
+    {
+        ObjectiveManager.Instance.OnObjectiveCompleted.AddListener(IncreaseSpookLikelyhood);
+    }
+
+    void IncreaseSpookLikelyhood(){
+        spookOmeter++;
+    }
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if(timePassed > 5f)
+        {
+            timePassed -= Random.Range(5f, 15f);
+            if(Random.Range(0,100/(spookOmeter+1)) <= 2){
+                float time = Random.Range(.5f,3);
+                decals[Random.Range(0,decals.Length)].GetComponent<WindowDecalController>().SetActiveSeconds(time);
+            }
+        } 
+    }
+}
