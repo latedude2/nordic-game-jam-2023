52b8cd3e9320ab214dbc1e8412b23fc083785d17
latedude2 <simasc10@gmail.com>
2023-04-17 19:41:21 +0200

    Add delivery counter and adjust delivery info to fit name, age and job title

---
+++ Assets/Scripts/UI/DeliveryCounter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DeliveryCounter : MonoBehaviour
+{
+    private Text counterText;
+    private int deliveries = 0;
+    void Start()
+    {
+        ObjectiveManager.Instance.OnObjectiveCompleted.AddListener(UpdateCounter);
+        counterText = GetComponent<Text>();
+        counterText.text = "";
+    }
+
+    void UpdateCounter()
+    {
+        deliveries++;
+        counterText.text = deliveries.ToString();
+    }
+}
