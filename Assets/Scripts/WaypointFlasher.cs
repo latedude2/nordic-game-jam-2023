ba7ea4effd89c5ebc2603e0dbcc3d19631dcffec
Mikkel Munk <cloaked@live.dk>
2024-01-28 11:54:13 +0100

    More waypoint

--- Assets/Scripts/WaypointFlasher.cs
+++ Assets/Scripts/WaypointFlasher.cs
@@ -7,6 +7,7 @@ public class WaypointFlasher : MonoBehaviour
     Renderer rend;
     Color color;
     [SerializeField] float rate;
+    [SerializeField] float offset;
     float i;
 
     void Start()
@@ -17,7 +18,7 @@ public class WaypointFlasher : MonoBehaviour
     void Update()
     {
         i += Time.deltaTime*rate;
-        color = new Color(0.9433962f, 0.8425186f, 0.3070486f, Mathf.Lerp(0.03098039f ,0.16137f, Mathf.PingPong(i * 2, 1)));
+        color = new Color(0.9433962f, 0.8425186f, 0.3070486f, Mathf.Lerp(0.035f ,0.13f, Mathf.PingPong((i * 2) + offset, 1)));
         rend.material.SetColor("_Color", color);
     }
 }
