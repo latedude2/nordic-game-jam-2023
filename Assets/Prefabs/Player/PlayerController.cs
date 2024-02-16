f8419bcc5626ca47f98933bad4382ee57aba41a5
latedude2 <simasc10@gmail.com>
2024-02-16 08:06:53 +0100

    Work on the player controller

---
+++ Assets/Prefabs/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : NetworkBehaviour
+{
+    public Possessable PossessedObject;
+    public Possessable InitialPossessedPrefab;
+
+    private Vector2 Look;
+    private Vector2 Move;
+    private bool Interact;
+    private bool Jump;
+    private bool Crouch;
+
+
+    
+    public void OnLook(InputValue value)
+    {
+        Look = value.Get<Vector2>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Move = value.Get<Vector2>();
+    }
+
+    public void OnInteract(InputValue value)
+    {
+        Interact = value.isPressed;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        Jump = value.isPressed;
+    }
+
+    public void OnCrouch(InputValue value)
+    {
+        Crouch = value.isPressed;
+    }
+
+    
+
+}
---
+++ Assets/Prefabs/Player/Possessable.cs
@@ -0,0 +1,7 @@
+//interface for objects that can be possessed by the player
+public interface Possessable
+{
+    void Possess();
+    void Unpossess();
+
+};
