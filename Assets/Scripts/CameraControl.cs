8b06cf4223aea67c3becd8ad13a014b37d9a75f2
latedude2 <simasc10@gmail.com>
2024-02-18 14:08:43 +0100

    Setup looking over both shoulders

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
-{    public float sensitivity = 2.0f; // Camera sensitivity
+{    
+    public float sensitivity = 2.0f; // Camera sensitivity
     public float minimumY = -80.0f; // Minimum vertical angle
     public float maximumY = 80.0f; // Maximum vertical angle
+
+    public float minimumX = 180; // Maximum horizontal angle
+    public float maximumX = 210; // Minimum horizontal angle
+
+    public float minShoulderLook = 180;
+    public float maxShoulderLook = 270;
     private float cameraTruckAdjustment = 0.002f; 
+    public float positionOffset = 0;
+
+    [MaxValue(1)]
+    public float positionOffsetLeftFraction = 1f;
+    
+    [MaxValue(1)]
+    public float positionOffsetRightFraction = 1f;
+
+    public float rotationX = 0;
 
     private float rotationY = 0.0f;
     void Start()
@@ -26,40 +43,39 @@ public class CameraControl : MonoBehaviour
     void Update()
     {
         // Rotate the camera based on the mouse movement
-        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
 
         rotationY += Input.GetAxis("Mouse Y") * sensitivity;
         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
         //limit camera rotation to the left
-        if (rotationX < 280 && rotationX > 225)
+        if (rotationX < maximumX && rotationX > maximumX - 10)
         {
-            rotationX = 280;
+            rotationX = maximumX;
         }
-        else if (rotationX > 180 && rotationX < 225)
+        else if (rotationX > minimumX && rotationX < minimumX + 10)
         {
-            rotationX = 180;
+            rotationX = minimumX;
         }
 
         transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0.0f);
     
-        if(transform.localEulerAngles.y < 180)
-        {
-            //move camera to the right
-            transform.localPosition = new Vector3(transform.localEulerAngles.y * cameraTruckAdjustment, 0, 0);
-        }
-        else if(transform.localEulerAngles.y > 270)
-        {
-            //move camera to the left
-            transform.localPosition = new Vector3(0, 0, 0);
-        }
-        else
-        {
-            //move camera to the left
-            transform.localPosition = new Vector3(180 * cameraTruckAdjustment, 0, 0);
-        }
-
+        LookOverShoulder();
+        
     }
 
-    
+    void LookOverShoulder()
+    {
+        if(rotationX < minimumX)
+        {
+            positionOffset = rotationX * cameraTruckAdjustment * positionOffsetRightFraction;
+        }
+        else if(rotationX > maximumX)
+        {
+            positionOffset = (360 - rotationX) * cameraTruckAdjustment * -1 * positionOffsetLeftFraction;
+        }
+
+
+        transform.localPosition = new Vector3(positionOffset, 0, 0);
+    }
 }
